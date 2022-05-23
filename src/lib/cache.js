e8d48648deb3509bfc45307ed9b5bc4607b1dceb
jhonatajr <48875248+jrdevn@users.noreply.github.com>
2022-05-23 20:42:00 -0300

    initial archives

---
+++ src/lib/cache.js
@@ -0,0 +1,38 @@
+const Redis = require('ioredis');
+
+class Cache {
+
+    constructor() {
+        this.redis = new Redis({
+            host: process.env.REDIS_HOST || "localhost",
+            port: process.env.REDIS_PORT || 6379,
+            keyPreFix: "cache:"
+        })
+    }
+
+    async get(key) {
+        const value = await this.redis.get(key);
+        return value ? JSON.parse(value) : null;
+    }
+
+    set(key, value, timeExpiration) {
+        return this.redis.set(key, JSON.stringify(value), timeExpiration);
+    }
+
+    del(key) {
+        return this.redis.del(key);
+    }
+
+
+    async delPreFix(prefix) { // aqui ele deleta pelos prefixos
+        const keys =  (await this.redis.keys(`cache:${prefix}:*`)).map(key => 
+            key.replace("cache: ", ""));
+
+        
+        return this.redis.del(keys); // cache:id:* 
+
+    }
+
+}
+
+export default new Cache();
